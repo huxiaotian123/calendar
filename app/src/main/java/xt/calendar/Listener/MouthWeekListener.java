d41891ae97fb9ccb6f5371f3a7576feffc35de34
abel <491494883@qq.com>
2016-10-13 23:49:56 +0800

    Initial commit

---
+++ app/src/main/java/xt/calendar/Listener/MouthWeekListener.java
@@ -0,0 +1,10 @@
+package xt.calendar.Listener;
+
+/**
+ * Created by Administrator on 2016/10/13.
+ * 展示月份和周的监听
+ */
+public interface MouthWeekListener {
+    void showMouthView();
+    void showWeekView();
+}
---
+++ app/src/main/java/xt/calendar/widget/VariableViewPager.java
@@ -0,0 +1,81 @@
+package xt.calendar.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import xt.calendar.Listener.MouthWeekListener;
+
+/**
+ * Created by Administrator on 2016/10/13.
+ */
+public class VariableViewPager extends ViewPager  {
+
+    private float deviation = 200;//偏移量
+    private float startY; //起点位置
+    private boolean isMoth = true;//默认是下面
+    private MouthWeekListener mouthWeekListener;
+
+    public VariableViewPager(Context context) {
+        super(context);
+    }
+
+    public VariableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                startY = ev.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+            case MotionEvent.ACTION_UP:
+                float moveY = ev.getY() - startY;
+
+                if(Math.abs(moveY)>deviation){
+                    if(moveY > 0 ){//下移
+                        showMouth();
+                    }else {//上移
+                        showWeek();
+                    }
+                }
+                break;
+
+        }
+        return true;
+    }
+
+    /**
+     * 上移
+     */
+    private void showWeek() {
+        if(isMoth){
+            LinearLayout.LayoutParams LayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,200);
+            this.setLayoutParams(LayoutParams);
+            isMoth = false;
+            if(null!= mouthWeekListener){
+                mouthWeekListener.showWeekView();
+            }
+        }
+    }
+
+    /**
+     * 需要下移
+     */
+    private void showMouth() {
+        if(!isMoth){
+            LinearLayout.LayoutParams LayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
+            this.setLayoutParams(LayoutParams);
+            isMoth = true;
+            if(null!= mouthWeekListener){
+                mouthWeekListener.showMouthView();
+            }
+        }
+    }
+}
